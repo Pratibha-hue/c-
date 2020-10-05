e878a708bd627cc7d7833bc889d8a8e43edea43c
Pratibha bhole <66415382+Pratibha-hue@users.noreply.github.com>
2020-10-05 23:43:56 +0530

    Update product of 2 no.s

--- product of 2 no.s
+++ product of 2 no.s
@@ -1,3 +1,24 @@
+programme 1-
+#include<iostream>
+using namespace std;
+int main(){
+  int x=1,y=4,z=8;
+  
+	cout<<"Size of char:"<<x<<" bytes\n";
+
+	cout<<"Size of int:"<<y<<" bytes\n";
+	
+	cout<<"Size of float:"<<y<<" bytes\n";
+
+	cout<<"Size of double:"<<z<<" bytes";
+	
+	return 0;
+	
+	}
+
+
+programme 2-
+
 #include<iostream>
 using namespace std;
 int main(){
