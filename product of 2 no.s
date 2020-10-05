4a35ad0d25a2b1b7e970272e5dc8ca1ca11db74e
Pratibha bhole <66415382+Pratibha-hue@users.noreply.github.com>
2020-10-05 23:32:54 +0530

    Create product of 2 no.s

---
+++ product of 2 no.s
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x,y,z;
+	cout<<"enter two numbers:";
+	cin>>x>>y;
+	z=x*y;
+	cout<<"product of two number is:"<<z;
+	return 0;
+	
+	
+}
